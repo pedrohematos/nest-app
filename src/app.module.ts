9a620a9c3c5e4e946e1872a434828a2ce30d2ce8
Pedro Matos <pedrohematos@outlook.com>
2022-01-05 15:29:12 -0300

    implement notification service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
